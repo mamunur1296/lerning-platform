31d1821c7d4cfb3ed270c8347bf2e787c31d523a
mamunur1296 <programer597@gmail.com>
2022-10-25 10:54:00 +0600

    fack data setap

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PROT || 5000;
+const cources = require("./data/cources.json");
+
+app.get("/allcources", (req, res) => {
+  res.send(cources);
+});
 
 app.get("/", (req, res) => {
   res.send("hallo wold");
