890dde37290e39b37fa2f9ce0c038826507a0bea
mamunur1296 <programer597@gmail.com>
2022-10-25 12:34:27 +0600

    catagory data add now

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PROT || 5000;
 const cources = require("./data/cources.json");
+const courcesCatagory = require("./data/corcesCatagory.json");
 
 app.use(cors());
 app.get("/allcources", (req, res) => {
   res.send(cources);
+});
+app.get("/corcesCatagory", (req, res) => {
+  res.send(courcesCatagory);
 });
 
 app.get("/", (req, res) => {
